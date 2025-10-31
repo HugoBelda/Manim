2c78a628bcbe723a7c3dc861d3d8fc96b0eebcf6
Hugo Belda <hugobeldarevert@icloud.com>
2025-10-31 21:51:20 +0100

    Add scene.py with SquareToCircle and AnimatedSquareToCircle classes

---
+++ my-project/scene.py
@@ -0,0 +1,26 @@
+from manim import *
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # Creates a circle
+        # sets the color anf the transparency
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, LEFT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(Transform(square, circle))  # transform the square into a circle
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
